6d9a4f4690737b04c57119c29413b9e94d573a39
kennywf <63686149+kennywf@users.noreply.github.com>
2020-09-16 22:56:52 -0400

    adding links to maps

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoieWFsY2EwMDciLCJhIjoiY2tlZnJlbnY1MThlNzJ5cDNlbWl0cjJtNyJ9.1c2h9gZ4VceBQJfA4OwUQg"
+const API_KEY = "pk.eyJ1Ijoia3d1ZmVuZyIsImEiOiJja2VmcGM1MG0wYWFoMnlxb3prbjc2cXltIn0.yjIryPg9B3oJx5U60NLoAw"
